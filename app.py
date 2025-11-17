d2f980f1e3adcb96031fbeeccd21d0b4f436578e
citraaaulia <citraauliax@gmail.com>
2025-11-17 17:03:05 +0700

    last edit app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_app():
     app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '127.0.0.1')
     app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
     app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
-    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'railway')
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'db_ml_chaos')
     app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', '3306'))
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
